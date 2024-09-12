5dfbc97d017a4616148ab8f7d4b9e6f735db70e0
DenisCNS <138787359+DenisCNS@users.noreply.github.com>
2024-09-12 15:15:25 +0300

    Update Circle.cs

--- Solution One/Figures/Circle.cs
+++ Solution One/Figures/Circle.cs
@@ -17,9 +17,6 @@ namespace SquareLibrary.Figures
 
         public override double GetSquare() => Math.PI * Math.Pow(_radius, 2);
 
-        private bool IsCircleExist(double radius) 
-        {
-            return radius > 0;
-        }
+        private bool IsCircleExist(double radius) => radius > 0;        
     }
 }
